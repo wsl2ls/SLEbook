e629b131f220b2efc2028a2fff37d4d68fd8a9e9
wsl <15324956576@163.com>
2019-12-19 09:57:54 +0800

    coreText下划线

--- EPudRead/EPudRead/SLCoreTextView.h
+++ EPudRead/EPudRead/SLCoreTextView.h
@@ -17,7 +17,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, strong) NSArray <SLImageData *> * imageArray;
 
-@property (nonatomic, strong) NSArray * linkRanges; //所有的链接
+@property (nonatomic, strong) NSMutableArray * linkRanges; //所有的链接
+@property (nonatomic, strong) NSMutableArray *underlineRanges; //所有下划线
 
 @property (nonatomic, assign, readonly) CGFloat textHeight; //富文本的高度
 
